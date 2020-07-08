451f4d3d07fe47120e2da75342c4b1fbb9482edf
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2020-07-08 08:03:49 -0300

    Aplicação de CSS ao arquivo

--- src/basico/Contador.java
+++ src/basico/Contador.java
@@ -11,13 +11,27 @@ import javafx.stage.Stage;
 
 public class Contador extends Application {
 
+    private int contador = 0;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         Label labelTitulo = new Label("Contador");
+        labelTitulo.getStyleClass().add("titulo");
         Label labelNumero = new Label("0");
+        labelNumero.getStyleClass().add("numero");
 
         Button botaoDecremento = new Button("-");
+        // .setOnAction() é o evento que será disparado, ao clicar no botão
+        botaoDecremento.setOnAction(e -> {
+            contador--;
+            labelNumero.setText(Integer.toString(contador));
+        });
+
         Button botaoIncremento = new Button("+");
+        botaoIncremento.setOnAction(e -> {
+            contador++;
+            labelNumero.setText(Integer.toString(contador));
+        });
 
         HBox boxBotoes = new HBox();
         boxBotoes.setAlignment(Pos.CENTER);
@@ -26,16 +40,21 @@ public class Contador extends Application {
         boxBotoes.getChildren().add(botaoDecremento);
         boxBotoes.getChildren().add(botaoIncremento);
 
-        VBox boxPrincipal = new VBox();
-        boxPrincipal.setAlignment(Pos.CENTER);
-        boxPrincipal.setSpacing(10);
+        VBox boxConteudo = new VBox();
+        boxConteudo.getStyleClass().add("conteudo");
+        boxConteudo.setAlignment(Pos.CENTER);
+        boxConteudo.setSpacing(10);
         // Adicionando Elementos ao VBox
-        boxPrincipal.getChildren().add(labelTitulo);
-        boxPrincipal.getChildren().add(labelNumero);
-        boxPrincipal.getChildren().add(boxBotoes);
+        boxConteudo.getChildren().add(labelTitulo);
+        boxConteudo.getChildren().add(labelNumero);
+        boxConteudo.getChildren().add(boxBotoes);
 
+        // Importar arquivo css
+        String caminhoDoCss = getClass().getResource("/basico/Contador.css").toExternalForm();
         // Carregar o VBox dentro da Scene
-        Scene cenaPrincipal = new Scene(boxPrincipal, 400, 400);
+        Scene cenaPrincipal = new Scene(boxConteudo, 400, 400);
+        cenaPrincipal.getStylesheets().add(caminhoDoCss);
+        cenaPrincipal.getStylesheets().add("https://fonts.googleapis.com/css2?family=Oswald");
         // Carregar Scene no Stage (palco)
         primaryStage.setScene(cenaPrincipal);
         primaryStage.show();
